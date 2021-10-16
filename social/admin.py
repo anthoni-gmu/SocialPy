d6c1a66e5df55ec1336714f82b71187661977023
anthoni portocarrero rodriguez <61163166+anthoni-gmu@users.noreply.github.com>
2021-10-15 22:56:03 -0500

    post

--- social/admin.py
+++ social/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import SocialPost,SocialComment ,Image
 
-# Register your models here.
+admin.site.register(SocialPost)
+admin.site.register(SocialComment)
+admin.site.register(Image)
+
+
+#min 30
--- social/models.py
+++ social/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth import get_user_model
+User=get_user_model()
 
-# Create your models here.
+def user_directory_path(instance,filename):
+    return 'users/socialpost/{0}'.format(filename)
+
+def dm_directory_path(instance,filename):
+    return 'users/messages/{0}'.format(filename)
+
+class SocialPost(models.Model):
+    body=models.TextField()
+    image=models.ManyToManyField('Image',blank=True)
+    create_on = models.DateTimeField(default=timezone.now)
+    author=models.ForeignKey(User,on_delete=models.CASCADE,related_name='social_post_author')
+    likes=models.ManyToManyField(User,blank=True,related_name='likes')
+    dislike=models.ManyToManyField(User,blank=True,related_name='dislike')
+    
+class SocialComment(models.Model):
+    commet=models.TextField() 
+    create_on = models.DateTimeField(default=timezone.now)
+    author=models.ForeignKey(User,on_delete=models.CASCADE,related_name='social_comment_author')
+    likes=models.ManyToManyField(User,blank=True,related_name='comment_likes')
+    dislike=models.ManyToManyField(User,blank=True,related_name='comment_dislike')
+    
+class Image(models.Model):
+    image=models.ImageField(upload_to=user_directory_path,blank=True,null=True)
