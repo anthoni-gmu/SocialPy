f1f34c982dafa230bdefbdcb12fa37939e2968e6
anthoni portocarrero rodriguez <61163166+anthoni-gmu@users.noreply.github.com>
2021-10-21 01:56:42 -0500

    detail-comments

--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import TemplateView, View
-from django.shortcuts import redirect, render, get_object_or_404, redirect
+from django.shortcuts import redirect, render, get_object_or_404
 from django.core.paginator import Paginator
 from django.contrib.auth.mixins import UserPassesTestMixin,LoginRequiredMixin
 from social.forms import SocialPostForm
--- social/models.py
+++ social/models.py
@@ -18,11 +18,23 @@ class SocialPost(models.Model):
     dislikes=models.ManyToManyField(User,blank=True,related_name='dislikes')
     
 class SocialComment(models.Model):
-    commet=models.TextField() 
+    comment =models.TextField() 
     create_on = models.DateTimeField(default=timezone.now)
     author=models.ForeignKey(User,on_delete=models.CASCADE,related_name='social_comment_author')
+    post=models.ForeignKey('SocialPost',on_delete=models.CASCADE)
     likes=models.ManyToManyField(User,blank=True,related_name='comment_likes')
-    dislike=models.ManyToManyField(User,blank=True,related_name='comment_dislikes')
+    dislikes=models.ManyToManyField(User,blank=True,related_name='comment_dislikes')
+    parent=models.ForeignKey('self',on_delete=models.CASCADE,blank=True,null=True,related_name='+')
+    
+    @property
+    def children(self):
+        return SocialComment.objects.filter(parent=self).order_by('-create_on').all()
+    
+    @property
+    def is_parent(self):
+        if self.parent is None:
+            return True
+        return False
     
 class Image(models.Model):
     image=models.ImageField(upload_to=user_directory_path,blank=True,null=True)
